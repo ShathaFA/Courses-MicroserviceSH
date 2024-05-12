f9e035d572ac162896a8149910423406987d7193
ShathaFA <105555472+ShathaFA@users.noreply.github.com>
2024-05-12 04:32:02 +0300

    Update settings.py

--- Courses-Microservice-main/Courses/Courses/settings.py
+++ Courses-Microservice-main/Courses/Courses/settings.py
@@ -138,8 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000  # adjust the number based on your requirement
 
-SITE_DOMAIN = 'http://54.146.212.138:8001'
-USERS_SERVICE_URL = 'http://54.146.212.138:8000'
+SITE_DOMAIN = 'http://127.0.0.1:8001'
+USERS_SERVICE_URL = 'http://127.0.0.1:8000/'
 CORS_ALLOW_ALL_ORIGINS = True  # For development only
 CORS_ALLOWED_ORIGINS = [USERS_SERVICE_URL]
 
